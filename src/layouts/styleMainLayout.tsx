03266a06d9b3cd803ad20805510ce22bc5389251
Alexander-Konstantinovich <shushpanov.ak@yandex.ru>
2024-07-10 20:20:33 +0300

    feat: add action for table

--- src/layouts/styleMainLayout.tsx
+++ src/layouts/styleMainLayout.tsx
@@ -6,5 +6,4 @@ export const DivLayout = styled.div<{ mode?: string }>`
 export const DivLayoutOutlet = styled.div<{ mode?: string }>`
   margin-left: auto;
 	margin-right: auto;
-	
 `
--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react"
-import { Table } from "antd"
+import { Space, Table, Typography } from "antd"
 import type { TableColumnsType } from "antd"
 import { useAppSelector, useAppDispatch } from "../redux/hooks"
 import { selectTable } from "../redux/table/selectors"
 import { fetchAddProducts } from "../redux/table/asyncActions"
 import styled from "styled-components"
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons"
+import type { TableItem } from "../redux/table/types"
 
 export const TableColumns = styled.div<{ mode?: string }>`
   :where(.css-dev-only-do-not-override-f7vrd6).ant-table-wrapper .ant-table {
@@ -13,33 +14,32 @@ export const TableColumns = styled.div<{ mode?: string }>`
   }
 `
 
-interface TypeColumn {
-  key: React.Key
-  price: number
-  category: string
-}
-
-const columns: TableColumnsType<TypeColumn> = [
+const columns: TableColumnsType<TableItem> = [
   {
     title: "Id",
     dataIndex: "id",
     key: "id",
+    width: "50px",
   },
   {
     title: "Title",
     dataIndex: "title",
     key: "title",
+    width: "20%"
   },
   {
     title: "Price",
     dataIndex: "price",
     key: "price",
+    defaultSortOrder: 'descend',
+    width: "7%",
     sorter: (a, b) => a.price - b.price,
   },
   {
     title: "Category",
     dataIndex: "category",
     key: "category",
+    width: "10%",
     showSorterTooltip: { target: "full-header" },
     filters: [
       {
@@ -66,11 +66,24 @@ const columns: TableColumnsType<TypeColumn> = [
     title: "Description",
     dataIndex: "description",
     key: "description",
+    ellipsis: true,
   },
   {
     title: "Image",
     dataIndex: "image",
     key: "image",
+    ellipsis: true,
+  },
+  {
+    title: 'Action',
+    width: 100,
+    fixed: 'right',
+    render: () => (
+      <Space>
+        <Typography.Link><EditOutlined/></Typography.Link>
+        <Typography.Link><DeleteOutlined/></Typography.Link>
+      </Space>
+    ),
   },
 ]
 
